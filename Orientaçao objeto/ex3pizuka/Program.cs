ca8f993cf927e73798c8f01320c3f9c65fc5bc25
Raul Scapin Do Nascimento <46791749865@info.local>
2019-04-22 11:31:34 -0300

    fim da parte 1 da pizzaria

--- Orientaçao objeto/ex3pizuka/Program.cs
+++ Orientaçao objeto/ex3pizuka/Program.cs
@@ -12,46 +12,82 @@ namespace ex3pizuka
             bool sair = false;
             Usuario[] usuarios = new Usuario[5];
             int contasCriadas = 0;
+            int cont = 1;
             do{
                 System.Console.WriteLine("\n------Menu------\n| 1 - Cadastrar usuários\n| 2 - Efetuar login\n| 3 - Listar usuários\n| 9 - Sair");                
                 int escolha = int.Parse(Console.ReadLine());
                 switch (escolha)
                 {
                     case 1://cadastrar usuários
-                        Usuario u = new Usuario();
-
-                        System.Console.WriteLine("Insira o nome:");
+                        System.Console.Write("Insira o nome: ");
                         string nome = Console.ReadLine();
                         
                         string email;
                         do{
-                            System.Console.WriteLine("Insira o email:");
+                            System.Console.Write("Insira o email: ");
                             email = Console.ReadLine();
-                            if (email.IndexOf("@") >=0){
-                                System.Console.WriteLine("tem @");
-                            } else{
+                            if (email.IndexOf("@") <=0 || email.IndexOf(".") <=0){
                                 System.Console.WriteLine("O email deve conter @ e .");
                             }
                         } while (email.IndexOf("@") <=0 || email.IndexOf(".") <=0);
-                        
-                        System.Console.WriteLine("Insira o senha:");
-                        string senha = Console.ReadLine();
-                        contasCriadas++;
-                        u.Criacao(nome,email,senha,contasCriadas);
 
-                        
+                        string senha;
+                        do{
+                            System.Console.Write("Insira o senha: ");
+                            senha = Console.ReadLine();
+                            if (senha.Length < 6){
+                                System.Console.WriteLine("A senha deve conter mais de 6 caracteres");
+                            }
+                        } while (senha.Length < 6);
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        System.Console.WriteLine(usuarios[contasCriadas].Inserir(nome,email,senha,cont));
+                        Console.ResetColor ();
+
+                        contasCriadas++;
+                        cont++;
                     break;
 
                     case 2://efetuar login
-                    
+                        System.Console.WriteLine("---Área de Login---");
+                        System.Console.Write("Digite o e-mail: ");
+                        string emailUser = Console.ReadLine();
+                        Usuario userRecuperado = null;
+                        foreach (Usuario item in usuarios)
+                        {
+                            if (item != null && emailUser.Equals(item.email)) {
+                                userRecuperado = item;
+                                break;
+                            }
+                        }
+                        if (userRecuperado == null){
+                            System.Console.WriteLine("E-mail não cadastrado.");
+                            continue;
+                        }
+                        
+                        System.Console.Write("Digite a senha: ");
+                        string senhaUser = Console.ReadLine();
+
+                        System.Console.WriteLine(userRecuperado.EfetuarLogin(senhaUser));
+                        Console.ResetColor ();
                     break;
                     
                     case 3://listar usuários
-                    
+                        int cont3 =0;
+                        foreach (Usuario item in usuarios){
+                            if (item == null){
+                                break;
+                            }else{
+                                cont3++;
+                                System.Console.WriteLine(item.Listar(cont3));
+                            }
+                        }
                     break;
 
                     case 9: //sair
+                        Console.ForegroundColor = ConsoleColor.Green;
                         System.Console.WriteLine("Obrigado pela atenção...");
+                        Console.ResetColor();
                         sair = true;
                     break;
                 } //fim switch
--- Orientaçao objeto/ex3pizuka/Usuario.cs
+++ Orientaçao objeto/ex3pizuka/Usuario.cs
@@ -3,20 +3,33 @@ namespace ex3pizuka
     using System;
     public class Usuario
     {
-        public int id;
+        public int id = 0;
         public string nome;
         public string email;
         private string senha;
-        public DateTime dataCriacao;
+        private DateTime dataCriacao;
 
-        public string Criacao(string nome, string email, string senha, int cont){
+        public string Inserir(string nome, string email, string senha, int cont){
             this.nome = nome;
             this.email = email;
             this.senha = senha;
+            dataCriacao = DateTime.Now;
             id = cont;
             return "Usuario criado com sucesso.";
         }
-
-
+        public string Listar(int cont3){
+            string mensagem ="";
+            mensagem+="-----Usuário "+cont3+"---\nNome do usuário: "+nome+"\nE-mail: "+email+"\nId: "+id+"\n";
+            return mensagem;
+        }
+        public string EfetuarLogin(string senhaUser){
+            if(senhaUser == senha){
+                Console.ForegroundColor = ConsoleColor.Green;
+                return $"Bem-vindo - {nome}";
+            }else{
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                return "Senha inválida.";
+            }
+        }
     }
 }
